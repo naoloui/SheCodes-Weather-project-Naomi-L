2109fb05541fc2e8f2cab03a69cea1a56b8d4860
naoloui <naomi.laufer16@gmail.com>
2025-05-09 22:26:21 +0200

    Updates on HTML and Javascript

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
+
   temperatureElement.innerHTML = Math.round(temperature);
-  cityElement.innerHTML = response.data.city;
 }
 
 function searchCity(city) {
-  let apiKey = "3bb429560a4tfe3ecf96fae66oed5d7f";
+  let apiKey = "3bb429560a4tfe3ecf96fae66oed5d7f"; // Ideally, secure this in environment variables
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  axios.get(apiUrl).then(refreshWeather).then(searchCity);
   console.log(apiUrl);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let = searchInput = document.querySelector("#search-form-input");
-  let = cityElement = document.querySelector("#city");
+  let searchInput = document.querySelector("#search-form-input"); // Fixed variable declaration
+  // Fixed variable declaration
+  let cityElement = document.querySelector("#city");
 
+  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
-  console.log(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
