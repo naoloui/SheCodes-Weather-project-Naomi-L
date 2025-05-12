a2a5548f2a917080e34ede55bbf11e5648818f81
naoloui <naomi.laufer16@gmail.com>
2025-05-12 19:06:30 +0200

    Removal of source in script html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector(".temperature");
   let temperature = response.data.temperature.current;
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   console.log(response.data.condition.description);
 
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${date} ${hours}:${minutes}`;
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 }
 
 function searchCity(city) {
@@ -19,11 +47,35 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input"); // Fixed variable declaration
   // Fixed variable declaration
-  let cityElement = document.querySelector("#city");
+  let cityElement = document.querySelector(".city");
 
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌧️</div>
+          <div class="weather-forecast-temperatures">
+            <strong>18ºC</strong>
+          </div>
+          <div class="weather-forecast-temperature">9ºC</div>
+        </div>`;
+  });
+}
+
+let forecastElement = document.querySelector(".forecast");
+forecastElement.innerHTML = forecastHtml;
+
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Madrid");
+displayForecast();
